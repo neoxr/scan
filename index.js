f4289553a9d8150c5f90365e37006cdf29542b67
neoxr <neoxr@null.net>
2022-10-21 18:55:09 +0700

    release

--- index.js
+++ index.js
@@ -1,107 +1,53 @@
-const pino = require('pino')
-const qrcode = require('qrcode-terminal')
-const fs = require('fs')
-const {
-   Boom
-} = require('@hapi/boom')
-const {
-   default: makeWASocket,
-   delay,
-   makeInMemoryStore,
-   useMultiFileAuthState,
-   makeCacheableSignalKeyStore,
-   DisconnectReason,
-   msgRetryCounterMap
-} = require('baileys')
-const filename = extension => {
-   return `${Math.floor(Math.random() * 10000)}.${extension}`
-}
-const sessionFile = 'session.json' // filename('json')
-const logger = pino().child({
-   level: 'silent'
-})
+const { default: Socket, useMultiFileAuthState, makeInMemoryStore, DisconnectReason, delay } = (await import('baileys')).default
+import { Boom } from '@hapi/boom'
+import * as ws from 'ws'
+import p from 'pino'
+import fs from 'fs'
+const logger = p({ level: 'silent' })
+const store = makeInMemoryStore({ logger })
 
-global.store = makeInMemoryStore({
-   logger
-})
-// store.readFromFile(sessionFile)
-
-const connect = async () => {
-   const {
-      state
-   } = await useMultiFileAuthState('session')
-   const client = makeWASocket({
-      logger: pino({
-         level: 'silent'
-      }),
-      printQRInTerminal: true,
-      auth: {
-         creds: state.creds,
-         /** caching makes the store faster to send/recv messages */
-         keys: makeCacheableSignalKeyStore(state.keys, logger),
-      },
-      version: [2, 2234, 13],
-      msgRetryCounterMap,
-      generateHighQualityLinkPreview: true,
-      getMessage: async (key) => {
-         return await store.loadMessage(client.decodeJid(key.remoteJid), key.id)
-      }
-   })
-
-   store.bind(client.ev)
-   client.ev.on('connection.update', async update => {
-      const {
-         connection,
-         lastDisconnect
-      } = update
-      console.log(update)
-      if (connection === 'connecting') console.log('sabar ngab lagi nyoba menghubungkan!')
-      if (connection === 'close') {
-         let reason = new Boom(lastDisconnect.error).output.statusCode
-         if (reason === DisconnectReason.badSession) {
-            console.log(`Bad Session, reconnecting...`)
-            connect()
-         } else if (reason === DisconnectReason.connectionClosed) {
-            console.log("Connection closed, reconnecting....")
-            connect()
-         } else if (reason === DisconnectReason.connectionLost) {
-            console.log("Connection Lost from Server, reconnecting...")
-            connect()
-         } else if (reason === DisconnectReason.connectionReplaced) {
-            console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First")
-            client.logout()
-         } else if (reason === DisconnectReason.loggedOut) {
-            console.log(`Device Logged Out, Please Scan Again And Run.`)
-            client.logout()
-         } else if (reason === DisconnectReason.restartRequired) {
-            console.log("Restart Required, Restarting...")
-            connect()
-         } else if (reason === DisconnectReason.timedOut) {
-            console.log("Connection TimedOut, Reconnecting...")
-            connect()
-         } else if (reason === DisconnectReason.multideviceMismatch) {
-            console.log("Multi device mismatch, please scan again")
-            client.logout()
-         } else client.end(`Unknown DisconnectReason: ${reason}|${connection}`)
-      }
-
-      if (update.isOnline) {
-         console.log('Connected!')
-         fs.writeFileSync(sessionFile, JSON.stringify(state, null, 3), 'utf-8')
-         await delay(1000 * 5)
-         client.sendMessage(client.user.id, {
-            document: {
-               url: `./${sessionFile}`
-            },
-            fileName: 'session.json',
-            mimetype: 'application/json'
-         }).then(async () => {
-            fs.unlinkSync(`./${sessionFile}`)
-            await delay(1000 * 10)
-            process.exit(0)
-         })
-      }
-   })
+const connect = () => {
+   try {
+      const { state, saveCreds } = await useMultiFileAuthState('Auth')
+      const client = Socket({
+         browser: ['© neoxr-bot', 'safari', '1.0.0'],
+         printQRInTerminal: true,
+         logger,
+         auth: state
+      })
+      store.bind(client.ev)
+      client.ev.on('connection.update', async (update) => {
+         const {
+            lastDisconnect,
+            connection
+         } = update
+         try {
+            if (connection == 'close') {
+               if (new Boom(lastDisconnect.error).output?.statusCode === DisconnectReason.loggedOut) connect()
+               else connect()
+            } else if (connection == 'open') {
+               console.log('Connected!')
+               await delay(1000 * 5)
+               client.sendMessage(client.user.id, {
+                  document: {
+                     url: `./Auth/creds.json`
+                  },
+                  fileName: 'creds.json',
+                  mimetype: 'application/json'
+               }).then(async () => {
+                  fs.unlinkSync(`./Auth/creds.json`)
+                  await delay(1000 * 10)
+                  process.exit(0)
+               })
+            }
+         } catch (e) {
+            console.log(e)
+         }
+      })
+      client.ev.on('creds.update', saveCreds)
+   } catch (e) {
+      console.log(e)
+   }
 }
 
 connect().catch(() => connect())
